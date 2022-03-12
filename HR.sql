66a64242e6114637ec2bac9fa57a1ded0c1f484d
Elif Koç <53998707+kocelifk@users.noreply.github.com>
2022-03-12 19:09:43 +0300

    Update HR.sql

--- HR.sql
+++ HR.sql
@@ -1,69 +1,70 @@
---2. Þirketimizde halen çalýþmaya devam eden çalýþanlarý nlistesini getiren sorgu hangisidir? 
---Not:Ýþten çýkýþ tarihi boþ olanlar çalýþmaya devam eden çalýþanlardýr.
+https://medium.com/@kocelifk/sql-b3648218d30f
+
+--Åžirketimizde halen Ã§alÄ±ÅŸmaya devam eden Ã§alÄ±ÅŸanlarÄ± nlistesini getiren sorgu hangisidir? 
+--Not:Ä°ÅŸten Ã§Ä±kÄ±ÅŸ tarihi boÅŸ olanlar Ã§alÄ±ÅŸmaya devam eden Ã§alÄ±ÅŸanlardÄ±r.
 SELECT * FROM PERSON WHERE OUTDATE IS NULL
 
 
---3. Þirketimizde departman bazlý halen çalýþmaya devam eden çalýþan sayýsýný getiren sorguyu yazýnýz?
+--Åžirketimizde departman bazlÄ± halen Ã§alÄ±ÅŸmaya devam eden Ã§alÄ±ÅŸan sayÄ±sÄ±nÄ± getiren sorguyu yazÄ±nÄ±z?
 
 SELECT DEPARTMENT AS DEPARTMAN_ADI, COUNT(PERSON.ID) AS WORKER_PERSON_COUNT FROM PERSON
-INNER JOIN DEPARTMENT D ON D.ID = PERSON.DEPARTMENTID  --person ve departman tablolarýnýn baðlanmasý
-WHERE PERSON.OUTDATE IS NULL --halen çalýþmaya devam etmesi
-GROUP BY DEPARTMENT --departman bazlý
+INNER JOIN DEPARTMENT D ON D.ID = PERSON.DEPARTMENTID  --person ve departman tablolarÄ±nÄ±n baÄŸlanmasÄ±
+WHERE PERSON.OUTDATE IS NULL --halen Ã§alÄ±ÅŸmaya devam etmesi
+GROUP BY DEPARTMENT --departman bazlÄ±
 
---4. Þirketimizde departman bazlý halen çalýþmaya devam KADIN ve ERKEK sayýlarýný getiren sorguyu yazýnýz.
+--Åžirketimizde departman bazlÄ± halen Ã§alÄ±ÅŸmaya devam KADIN ve ERKEK sayÄ±larÄ±nÄ± getiren sorguyu yazÄ±nÄ±z.
 
-SELECT *, -- Departman adýný getirmesi için * 
-(SELECT COUNT(*) FROM PERSON WHERE DEPARTMENTID = DEPARTMENT.ID AND GENDER='E' AND  OUTDATE IS NULL) AS ERKEK_SAYISI, --halen çalýþmaya devam eden erkek sayýsý
-(SELECT COUNT(*) FROM PERSON WHERE DEPARTMENTID = DEPARTMENT.ID AND GENDER='K' AND  OUTDATE IS NULL) AS KADIN_SAYISI --halen çalýþmaya devam eden kadýn sayýsý
+SELECT *, -- Departman adÄ±nÄ± getirmesi iÃ§in * 
+(SELECT COUNT(*) FROM PERSON WHERE DEPARTMENTID = DEPARTMENT.ID AND GENDER='E' AND  OUTDATE IS NULL) AS ERKEK_SAYISI, --halen Ã§alÄ±ÅŸmaya devam eden erkek sayÄ±sÄ±
+(SELECT COUNT(*) FROM PERSON WHERE DEPARTMENTID = DEPARTMENT.ID AND GENDER='K' AND  OUTDATE IS NULL) AS KADIN_SAYISI --halen Ã§alÄ±ÅŸmaya devam eden kadÄ±n sayÄ±sÄ±
 FROM DEPARTMENT
 
 
---5. Þirketimizin Planlama departmanýna yeni bir þef atamasý yapýldý ve maaþýný belirlemek istiyoruz.
---Planlama departmaný içi nminimum, maximum ve ortalama þef maaþý getiren sorgu hangisidir? (Not:iþten çýkmýþ olan personel maaþlarý da dahildir.)
+--Åžirketimizin Planlama departmanÄ±na yeni bir ÅŸef atamasÄ± yapÄ±ldÄ± ve maaÅŸÄ±nÄ± belirlemek istiyoruz.
+--Planlama departmanÄ± iÃ§i nminimum, maximum ve ortalama ÅŸef maaÅŸÄ± getiren sorgu hangisidir? (Not:iÅŸten Ã§Ä±kmÄ±ÅŸ olan personel maaÅŸlarÄ± da dahildir.)
 
-SELECT POSITION.POSITION AS POZÝSYON , MIN(PERSON.SALARY) AS MÝNÝMUM_MAAS,
+SELECT POSITION.POSITION AS POZÄ°SYON , MIN(PERSON.SALARY) AS MÄ°NÄ°MUM_MAAS,
 MAX(PERSON.SALARY) AS MAXIMUM_MAAS,
 AVG(PERSON.SALARY) AS ORTALAMA_MAAS
 FROM POSITION INNER JOIN PERSON ON PERSON.POSITIONID = POSITION.ID
-WHERE POSITION = 'PLANLAMA ÞEFÝ'
+WHERE POSITION = 'PLANLAMA ÅžEFÄ°'
 GROUP BY POSITION.POSITION
 
 
---6. Her bir pozisyonda mevcut halde çalýþanlar olarak kaç kiþi ve ortalama maaþlarýnýn ne kadar olduðunu listelettirmek istiyoruz. 
---Sonucu getiren sorguyu yazýnýz.
-SELECT POSITION AS POZÝSYON, COUNT(*) AS KÝSÝ_SAYISI, AVG(PERSON.SALARY) AS ORTALAMA_MAAS 
+--Her bir pozisyonda mevcut halde Ã§alÄ±ÅŸanlar olarak kaÃ§ kiÅŸi ve ortalama maaÅŸlarÄ±nÄ±n ne kadar olduÄŸunu listelettirmek istiyoruz. 
+--Sonucu getiren sorguyu yazÄ±nÄ±z.
+SELECT POSITION AS POZÄ°SYON, COUNT(*) AS KÄ°SÄ°_SAYISI, AVG(PERSON.SALARY) AS ORTALAMA_MAAS 
 FROM POSITION INNER JOIN PERSON ON PERSON.POSITIONID = POSITION.ID
-GROUP BY POSITION ORDER BY KÝSÝ_SAYISI
+GROUP BY POSITION ORDER BY KÄ°SÄ°_SAYISI
 
 
---7.Yýllara göre iþe alýnan personel sayýsýný kadýn ve erkek bazýnda listelettiren sorguyu yazýnýz.
+--YÄ±llara gÃ¶re iÅŸe alÄ±nan personel sayÄ±sÄ±nÄ± kadÄ±n ve erkek bazÄ±nda listelettiren sorguyu yazÄ±nÄ±z.
 
-SELECT DISTINCT YEAR(PERSONTABLE.INDATE) YIL,  --INDATE: iþe giriþ tarihi; DISTINCT eklenmediði durumda ayný yýlý birden fazla kez gösteriyor
+SELECT DISTINCT YEAR(PERSONTABLE.INDATE) YIL,  --INDATE: iÅŸe giriÅŸ tarihi; DISTINCT eklenmediÄŸi durumda aynÄ± yÄ±lÄ± birden fazla kez gÃ¶steriyor
 (SELECT COUNT(*) FROM PERSON WHERE GENDER = 'E'AND YEAR(INDATE) = YEAR(PERSONTABLE.INDATE)) AS ERKEK_SAYISI,
 (SELECT COUNT(*) FROM PERSON WHERE GENDER = 'K' AND YEAR(INDATE) = YEAR(PERSONTABLE.INDATE)) AS KADIN_SAYISI
 FROM PERSON PERSONTABLE ORDER BY YIL
 
 
 
-
---8. Maaþ ortalamasý 5.500 TL’den fazla olan departmanlarý listeleyecek sorguyu yazýnýz. 
+--MaaÅŸ ortalamasÄ± 5.500 TLâ€™den fazla olan departmanlarÄ± listeleyecek sorguyu yazÄ±nÄ±z. 
 
 SELECT DEPARTMENT, AVG(PERSON.SALARY) AS ORTALAMA_MAAS FROM DEPARTMENT INNER JOIN PERSON ON PERSON.DEPARTMENTID = DEPARTMENT.ID
-GROUP BY DEPARTMENT.DEPARTMENT -- departman adýna göre 
-HAVING AVG(PERSON.SALARY) > 5500 --maaþ ortalamasý 5500 den fazla
+GROUP BY DEPARTMENT.DEPARTMENT -- departman adÄ±na gÃ¶re 
+HAVING AVG(PERSON.SALARY) > 5500 --maaÅŸ ortalamasÄ± 5500 den fazla
 
 
---9. Her personelin adýný, pozisyonunu baðlý olduðu birim yöneticisinin adýný ve pozisyonunu yönetici pozisyonunu getiren sorguyu yazýnýz.
+--Her personelin adÄ±nÄ±, pozisyonunu baÄŸlÄ± olduÄŸu birim yÃ¶neticisinin adÄ±nÄ± ve pozisyonunu yÃ¶netici pozisyonunu getiren sorguyu yazÄ±nÄ±z.
 SELECT 
 PER2.NAME_ + ' ' + PER2.SURNAME AS PERSONEL_AD_SOYAD, POS1.POSITION AS PERSONEL_POZISYON,
-PER1.NAME_ + ' ' + PER1.SURNAME AS YÖNETÝCÝ_ADI ,POS2.POSITION AS YÖNETÝCÝ_POZÝSYONU
+PER1.NAME_ + ' ' + PER1.SURNAME AS YÃ–NETÄ°CÄ°_ADI ,POS2.POSITION AS YÃ–NETÄ°CÄ°_POZÄ°SYONU
 FROM PERSON PER2
 INNER JOIN POSITION POS1 ON POS1.ID = PER2.POSITIONID --personelin pozisyonu
-INNER JOIN PERSON PER1 ON PER2.MANAGERID = PER1.ID --personelin yöneticisi
-INNER JOIN POSITION POS2 ON POS2.ID = PER1.POSITIONID --personelin yöneticisinin pozisyonu
+INNER JOIN PERSON PER1 ON PER2.MANAGERID = PER1.ID --personelin yÃ¶neticisi
+INNER JOIN POSITION POS2 ON POS2.ID = PER1.POSITIONID --personelin yÃ¶neticisinin pozisyonu
 ORDER BY PERSONEL_AD_SOYAD
 
---10. Departmanlarýn ortalama kýdemini ay olarak hesaplayacak sorguyu yazýnýz. 
+--DepartmanlarÄ±n ortalama kÄ±demini ay olarak hesaplayacak sorguyu yazÄ±nÄ±z. 
 
 
 SELECT  DEPARTMENT, AVG(WORKINGTIME) AS AYLIK_KIDEM
@@ -71,9 +72,9 @@ FROM
 (
 SELECT DEPARTMENT,
 
-CASE WHEN OUTDATE IS NOT NULL THEN DATEDIFF(MONTH, INDATE, OUTDATE) --ay bazýnda iþe giriþ ve iþten çýkýþ(iþten çýkmýþ olanlarý seçerek) kýdem hesaplamak
-ELSE DATEDIFF (MONTH,INDATE,GETDATE())-- ay bazýndan iþe giriþ tarihinden bugünün zamanýný(iþten çýkmamýþlarý hesaplamak için) baz alarak kýdem hesaplamak
+CASE WHEN OUTDATE IS NOT NULL THEN DATEDIFF(MONTH, INDATE, OUTDATE) --ay bazÄ±nda iÅŸe giriÅŸ ve iÅŸten Ã§Ä±kÄ±ÅŸ(iÅŸten Ã§Ä±kmÄ±ÅŸ olanlarÄ± seÃ§erek) kÄ±dem hesaplamak
+ELSE DATEDIFF (MONTH,INDATE,GETDATE())-- ay bazÄ±ndan iÅŸe giriÅŸ tarihinden bugÃ¼nÃ¼n zamanÄ±nÄ±(iÅŸten Ã§Ä±kmamÄ±ÅŸlarÄ± hesaplamak iÃ§in) baz alarak kÄ±dem hesaplamak
 
-END AS WORKINGTIME --aylýk kýdem hesaplama sonucunu working time deðiþkenine atamak
+END AS WORKINGTIME --aylÄ±k kÄ±dem hesaplama sonucunu working time deÄŸiÅŸkenine atamak
 FROM PERSON INNER JOIN DEPARTMENT ON PERSON.DEPARTMENTID = DEPARTMENT.ID) T GROUP BY DEPARTMENT
 
